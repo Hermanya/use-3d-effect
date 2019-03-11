44b8aebac5c6cfd5cfb6e2d8f94f943d5d2eb8b4
Herman Starikov <hermanstarikov@gmail.com>
2019-03-11 18:02:15 -0400

    Updates

---
+++ precache-manifest.62514fab776055484b9403b6f2410116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ea29ae4034dc123062de",
+    "url": "/static/js/main.93d7676d.chunk.js"
+  },
+  {
+    "revision": "08de86d1d0cc99b56d57",
+    "url": "/static/js/2.4b0b1626.chunk.js"
+  },
+  {
+    "revision": "ea29ae4034dc123062de",
+    "url": "/static/css/main.26b4c4d3.chunk.css"
+  },
+  {
+    "revision": "5126635fbc555dc3d7d956b682dd2679",
+    "url": "/index.html"
+  }
+];
